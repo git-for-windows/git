9f25a9ef60d19a2f826c836041f4c05029ca701e
Johannes Schindelin <johannes.schindelin@gmx.de>
2025-02-13 19:49:39 +0100

    fixup??? fscache: implement an FSCache-aware is_mount_point()
    Header refactoring. Yay.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- compat/mingw.h
+++ compat/mingw.h
@@ -41,7 +41,8 @@ static inline void convert_slashes(char *path)
 }
 struct strbuf;
 int mingw_is_mount_point(struct strbuf *path);
-#define is_mount_point mingw_is_mount_point
+extern int (*win32_is_mount_point)(struct strbuf *path);
+#define is_mount_point win32_is_mount_point
 #define PATH_SEP ';'
 char *mingw_query_user_email(void);
 #define query_user_email mingw_query_user_email
