597d3a58b99c52dd41294f9355e5f8ec1f3cc08a
Johannes Schindelin <johannes.schindelin@gmx.de>
2026-06-20 19:54:51 +0200

    fixup! mingw: explicitly specify with which cmd to prefix the cmdline

--- compat/mingw.c
+++ compat/mingw.c
@@ -2157,8 +2157,8 @@ static int is_msys2_sh(const char *cmd)
 }
 
 static pid_t mingw_spawnve_fd(const char *cmd, const char **argv, char **deltaenv,
-			      const char *dir, const char *prepend_cmd,
-			      int fhin, int fhout, int fherr)
+			      const char *dir,
+			      int prepend_cmd, int fhin, int fhout, int fherr)
 {
 	STARTUPINFOEXW si;
 	PROCESS_INFORMATION pi;
@@ -2238,9 +2238,9 @@ static pid_t mingw_spawnve_fd(const char *cmd, const char **argv, char **deltaen
 	/* concatenate argv, quoting args as we go */
 	strbuf_init(&args, 0);
 	if (prepend_cmd) {
-		char *quoted = (char *)quote_arg(prepend_cmd);
+		char *quoted = (char *)quote_arg(cmd);
 		strbuf_addstr(&args, quoted);
-		if (quoted != prepend_cmd)
+		if (quoted != cmd)
 			free(quoted);
 	}
 	for (; *argv; argv++) {
@@ -2360,8 +2360,7 @@ static pid_t mingw_spawnve_fd(const char *cmd, const char **argv, char **deltaen
 	return (pid_t)pi.dwProcessId;
 }
 
-static pid_t mingw_spawnv(const char *cmd, const char **argv,
-			  const char *prepend_cmd)
+static pid_t mingw_spawnv(const char *cmd, const char **argv, int prepend_cmd)
 {
 	return mingw_spawnve_fd(cmd, argv, NULL, NULL, prepend_cmd, 0, 1, 2);
 }
@@ -2389,14 +2388,14 @@ pid_t mingw_spawnvpe(const char *cmd, const char **argv, char **deltaenv,
 				pid = -1;
 			}
 			else {
-				pid = mingw_spawnve_fd(iprog, argv, deltaenv, dir, interpr,
+				pid = mingw_spawnve_fd(iprog, argv, deltaenv, dir, 1,
 						       fhin, fhout, fherr);
 				free(iprog);
 			}
 			argv[0] = argv0;
 		}
 		else
-			pid = mingw_spawnve_fd(prog, argv, deltaenv, dir, NULL,
+			pid = mingw_spawnve_fd(prog, argv, deltaenv, dir, 0,
 					       fhin, fhout, fherr);
 		free(prog);
 	}
@@ -2421,7 +2420,7 @@ static int try_shell_exec(const char *cmd, char *const *argv)
 		argv2[0] = (char *)cmd;	/* full path to the script file */
 		COPY_ARRAY(&argv2[1], &argv[1], argc);
 		exec_id = trace2_exec(prog, (const char **)argv2);
-		pid = mingw_spawnv(prog, (const char **)argv2, interpr);
+		pid = mingw_spawnv(prog, (const char **)argv2, 1);
 		if (pid >= 0) {
 			int status;
 			if (waitpid(pid, &status, 0) < 0)
@@ -2445,7 +2444,7 @@ int mingw_execv(const char *cmd, char *const *argv)
 		int exec_id;
 
 		exec_id = trace2_exec(cmd, (const char **)argv);
-		pid = mingw_spawnv(cmd, (const char **)argv, NULL);
+		pid = mingw_spawnv(cmd, (const char **)argv, 0);
 		if (pid < 0) {
 			trace2_exec_result(exec_id, -1);
 			return -1;
